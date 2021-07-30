8663e1dfeea6877de6c94578c37bd00e76384fa9
SrCobos <86936544+IvanCobos12@users.noreply.github.com>
2021-07-30 15:16:21 -0600

    Se agrego users

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+const user = [];
 
 app.get("/", (req,res)=>{
     res.send('Hello World!');
+})
+
+app.get("/users", (req,res) => {
+    res.send(users);
 })
 
 app.listen(3000, ()=>{
