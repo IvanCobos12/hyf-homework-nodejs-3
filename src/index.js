3c78a2f77c7f0385c9603f933c249c3675e81f39
SrCobos <86936544+IvanCobos12@users.noreply.github.com>
2021-08-02 11:52:20 -0600

    Se agrego metodo POST

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ app.get("/users", (req,res) => {
     res.send(users);
 })
 
+app.get("/users/:id", (req,res) => {
+    res.send(users);
+})
+
+app.post("/users/:id", (req,res)=>{
+    res.send('El usuario se ha creado');
+})
+
 app.listen(3000, ()=>{
     console.log('Server running');
 })
